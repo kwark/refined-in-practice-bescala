74c7242edfbb740a1a8c3898eb1fdf95fe8fce36
Peter Mortier <peter.mortier@gmail.com>
2018-04-30 15:46:33 +0200

    optimize simulacrum test

--- core/src/test/scala/RefinedSimulacrumOpsTest.scala
+++ core/src/test/scala/RefinedSimulacrumOpsTest.scala
@@ -1,4 +1,3 @@
-import RefinedOps.RefinedIntOps
 import RefinedSimulacrumOps._
 import eu.timepit.refined.scalacheck.numeric._
 import eu.timepit.refined.scalacheck.string.nonEmptyStringArbitrary
@@ -10,21 +9,10 @@ import org.scalatest.{FunSuite, Matchers}
 
 class RefinedSimulacrumOpsTest extends FunSuite with Matchers with PropertyChecks {
 
-  implicit val refinedStringStringOps = new RefinedStringOps[String] {
-    override def concat(t1: String, t2: String): String = t1 + t2
-  }
-
   import RefinedStringOps.ops._
-
-  test("String concat") {
-    forAll { (s1: String, s2: String) =>
-      (s1 |+| s2) shouldBe (s1 + s2)
-    }
-  }
 
   test("NonEmptyString concat") {
     forAll { (s1: NonEmptyString, s2: NonEmptyString) =>
-      val ev = implicitly[RefinedStringOps[NonEmptyString]]
       (s1 |+| s2).value.nonEmpty shouldBe true
     }
   }
@@ -33,7 +21,6 @@ class RefinedSimulacrumOpsTest extends FunSuite with Matchers with PropertyCheck
 
   test("PosInt add") {
     forAll { (i1: PosInt, i2: PosInt) =>
-      val ev = implicitly[RefinedIntOps[PosInt]]
       val result: Option[numeric.PosInt] = i1 |+| i2
       if (i2.value + i1.value > 0) result.nonEmpty shouldBe true
       else result.isEmpty shouldBe true
@@ -42,7 +29,6 @@ class RefinedSimulacrumOpsTest extends FunSuite with Matchers with PropertyCheck
 
   ignore("PosInt unsafeAdd") {
     forAll { (i1: PosInt, i2: PosInt) =>
-      val ev = implicitly[RefinedIntOps[PosInt]]
       // This test fails sometimes, because of int overflow
       (i1 !+! i2).value should be > 0
     }
